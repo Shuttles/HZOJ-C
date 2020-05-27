0c567f831ac2581fa8bfe690d561bb6ff73ffef7
chenbin <2287441467@qq.com>
2020-05-27 08:59:56 +0800

    单调队列

---
+++ 3.提高组/271.czy.2.cpp
@@ -0,0 +1,48 @@
+/*************************************************************************
+	> File Name: 271.czy.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 三  5/27 08:44:58 2020
+ ************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+#define MAX_N 300000
+int arr[MAX_N + 6], q[MAX_N + 6];
+int head = 0, tail = 0;
+
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+    for (int i = 1; i <= n; i++) cin >> arr[i];
+    
+    /*处理区间最小值, 维护一个单调递增队列*/
+    for (int i = 1; i <= n; i++) {
+        while (head - tail && arr[q[tail - 1]] >= arr[i]) --tail;
+        q[tail++] = i;
+        /*判断头部元素是否过期*/
+        if (q[head] <= i - k) head++;
+        if (i < k) continue;
+        (i == k) || cout << " ";
+        cout << arr[q[head]];
+    }
+
+    cout << endl;
+
+    head = tail = 0;
+    /*处理区间最大值, 维护一个单调递减队列*/
+    for (int i = 1; i <= n; i++) {
+        while (head - tail && arr[q[tail - 1]] <= arr[i]) --tail;
+        q[tail++] = i;
+        /*判断头部元素是否过期*/
+        if (q[head] <= i - k) head++;
+        if (i < k) continue;
+        (i == k) || cout << " ";
+        cout << arr[q[head]];
+    }
+
+    cout << endl;
+    return 0;
+}
